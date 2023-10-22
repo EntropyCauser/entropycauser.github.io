626dd0800b7a5bf6fa277a35d7589368ead40551
EntropyCauser <148713222+EntropyCauser@users.noreply.github.com>
2023-10-23 02:14:42 +0300

    Add files via upload

---
+++ testik/script.js
@@ -0,0 +1,9 @@
+document.getElementById('showDiv').addEventListener('click', function() {
+    var centeredDiv = document.getElementById('centeredDiv');
+    centeredDiv.style.display = 'block';
+});
+
+document.getElementById('closeDiv').addEventListener('click', function() {
+    var centeredDiv = document.getElementById('centeredDiv');
+    centeredDiv.style.display = 'none';
+});
